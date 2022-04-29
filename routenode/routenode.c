de8fab3f061a2ad8528f35a8e98ee5888b959d2c
Ian Choi <workhard2464@gmail.com>
2022-04-29 19:55:01 -0400

    Checkpoint

--- routenode/routenode.c
+++ routenode/routenode.c
@@ -13,7 +13,7 @@
 #include <math.h>
 
 struct node{int port,dist;};
-struct node routingTable[16];int size=0,sock,localPort,didBC=0;struct sockaddr_in my,to;socklen_t len=sizeof(struct sockaddr_in);struct timespec ts;
+struct node routingTable[16];int size=0,sock,localPort,didBC=0,isUpdated,isAdded;struct sockaddr_in my,to;socklen_t len=sizeof(struct sockaddr_in);struct timespec ts;
 
 void error(char *msg){
     perror(msg);
@@ -40,26 +40,44 @@ void printStatusMessages(int mode,int a,int b){
     }
 }
 
+void sortRoutingTable(void){
+    int i,j,low;struct node tmp;
+    
+    for(i=0;i<16;i++){
+        low=i;
+        if(routingTable[i].port==0)break;
+        for(j=i+1;j<16;j++){
+            if(routingTable[j].port==0)break;
+            if(routingTable[j].port<routingTable[low].port)low=j;
+        }
+        tmp=routingTable[i];
+        routingTable[i]=routingTable[low];
+        routingTable[low]=tmp;
+    }
+}
+
 void getRoutingTable(int argc,const char **argv){
+    struct node n;
+    
     for(int i=5;i<argc;i++){
-        struct node n;
         n.port=atoi(argv[i]);
         n.dist=atoi(argv[++i]);
         routingTable[size++]=n;
     }
+    sortRoutingTable();
     printStatusMessages(3,localPort,0);
 }
 
 void updateRoutingTable(struct node rt[],int fromPort){
-    int i,j,d=0;struct node rt_curr,curr;
+    int i,j,d=0;struct node rt_curr,curr;isUpdated=0;isAdded=0;
     
-    //find the distance from localPort -> fromPort
+    //find the distance from startNode -> midNode
     for(i=0;i<16;i++){
         curr=routingTable[i];
         if(curr.port==0)break;
         if(curr.port==fromPort)d=curr.dist;
     }
-    //for each toPort in rt, find toPort in routingTable and update the distance
+    //for each endNode in rt, find endNode in routingTable and update the distance(startNode -> endNode or startNode -> midNode -> endNode)
     for(i=0;i<16;i++){
         rt_curr=rt[i];
         if(rt_curr.port==0)break;if(rt_curr.port==localPort)continue;
@@ -68,12 +86,17 @@ void updateRoutingTable(struct node rt[],int fromPort){
             if(curr.port==0){
                 rt_curr.dist=d+rt_curr.dist;
                 routingTable[size++]=rt_curr;
+                isAdded=1;
                 break;
             }
             if(curr.port==rt_curr.port){
-                routingTable[j].dist=(int)fmin(curr.dist,d+rt_curr.dist);break;}
+                routingTable[j].dist=(int)fmin(curr.dist,d+rt_curr.dist);
+                if(routingTable[j].dist!=curr.dist)isUpdated=1;
+                break;
+            }
         }
     }
+    if(isAdded)sortRoutingTable();
 }
 
 void init(void){
@@ -119,7 +142,7 @@ void wait_rcv(void){
         printStatusMessages(2,localPort,ntohs(from.sin_port));
         updateRoutingTable(rt,ntohs(from.sin_port));
         printStatusMessages(3,localPort,0);
-        if(didBC==0)broadcast();
+        if(didBC==0 || (didBC==1 && isUpdated==1))broadcast();
     }
 }
 
