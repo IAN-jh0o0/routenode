58be2303a48698fe456212721de22362aeb6f9bd
Ian Choi <workhard2464@gmail.com>
2022-04-29 13:38:46 -0400

    Checkpoint

--- routenode/routenode.c
+++ routenode/routenode.c
@@ -9,58 +9,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include <netdb.h>
+#include <sys/time.h>
 
 struct node{
     int port,dist;
 };
-struct node routingTable[16];int size=0,sock;struct sockaddr_in my,to;socklen_t len=sizeof(struct sockaddr_in);
+struct node routingTable[16];int size=0,sock,localPort;struct sockaddr_in my,to;socklen_t len=sizeof(struct sockaddr_in);struct timespec ts;
 
 void error(char *msg){
     perror(msg);
     exit(0);
 }
 
-void init(int port){
-    struct hostent *hp;
-    //get socket
-    sock=socket(AF_INET,SOCK_DGRAM,0);
-    if(sock<0)error("sock() failed");
-    
-    //my sockaddr_in
-    bzero(&my,len);
-    my.sin_family=AF_INET;
-    my.sin_addr.s_addr=INADDR_ANY;
-    my.sin_port=htons(port);
-    if(bind(sock,(struct sockaddr*)&my,len)<0)error("bind() failed");
-    
-    //to sockaddr_in (only handle IP address)
-    to.sin_family=AF_INET;
-    hp=gethostbyname("localhost");
-    bcopy((char*)hp->h_addr,(char*)&to.sin_addr,hp->h_length);
-}
-
-//broadcast the routing information
-void broadcast(struct node *rt){
-    char buf[1024];int i,j;long n;
-    
-    //for each neighbor, send the information about other neightbors
-    for(i=0;i<size;i++){
-        to.sin_port=htons(rt[i].port);  //neighbor's port
-        for(j=0;j<size;j++){
-            if(i==j)continue;
-            sprintf(buf,"%d/%d",rt[j].port,rt[j].dist);  //neighbor/dist
-            n=sendto(sock,buf,strlen(buf),0,(struct sockaddr*)&to,len);  //send
-            if(n<0)error("sendto() failed");
-        }
+void printStatusMessages(int mode,int a,int b){
+    clock_gettime(CLOCK_REALTIME, &ts);
+    if(mode==1){
+        fprintf(stderr,"[%ld] Message sent from Node <port-%d> to Node <port-%d>\n",ts.tv_nsec,a,b);
     }
-}
-
-void wait_rcv(void){
-    struct sockaddr_in from;char buf[1024];
-    while(1){
-        long n=recvfrom(sock,buf,1024,0,(struct sockaddr*)&from,&len);
-        if(n<0)error("recvfrom() failed");
-        printf("%s\n",buf);
+    else if(mode==2){
+        fprintf(stderr,"[%ld] Message received at Node <port-%d> from Node <port-%d>\n",ts.tv_nsec,a,b);
     }
 }
 
@@ -73,9 +40,51 @@ void getRoutingTable(int argc,const char **argv){
     }
 }
 
+void init(void){
+    struct hostent *hp;
+    //get socket
+    sock=socket(AF_INET,SOCK_DGRAM,0);
+    if(sock<0)error("sock() failed");
+    
+    //my sockaddr_in
+    bzero(&my,len);
+    my.sin_family=AF_INET;
+    my.sin_addr.s_addr=INADDR_ANY;
+    my.sin_port=htons(localPort);
+    if(bind(sock,(struct sockaddr*)&my,len)<0)error("bind() failed");
+    
+    //to sockaddr_in (only handle IP address)
+    to.sin_family=AF_INET;
+    hp=gethostbyname("localhost");
+    bcopy((char*)hp->h_addr,(char*)&to.sin_addr,hp->h_length);
+}
+
+void broadcast(struct node *rt){
+    int i;long n;
+    
+    //for each neighbor, send the routing information
+    for(i=0;i<size;i++){
+        to.sin_port=htons(rt[i].port);  //neighbor's port
+        n=sendto(sock,routingTable,1024,0,(struct sockaddr*)&to,len);
+        if(n<0)error("sendto() failed");
+        printStatusMessages(1,localPort,rt[i].port);
+    }
+}
+
+void wait_rcv(void){
+    struct sockaddr_in from;struct node rt[16];
+    bzero(&rt,sizeof(rt));
+    
+    while(1){
+        long n=recvfrom(sock,rt,1024,0,(struct sockaddr*)&from,&len);
+        if(n<0)error("recvfrom() failed");
+        printStatusMessages(2,localPort,ntohs(from.sin_port));
+    }
+}
+
 int main(int argc,const char **argv){
-    int localPort=atoi(argv[4]);
-    init(localPort);
+    localPort=atoi(argv[4]);
+    init();
     if(strcmp(argv[argc-1],"last")==0){
         getRoutingTable(argc-1,argv);
         broadcast(routingTable);
